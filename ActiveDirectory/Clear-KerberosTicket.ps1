0012d37a0e4607dad77b03f5717d40ebacf6a4ef
Graeme B <grbray@outlook.com>
2020-06-29 14:42:18 -0500

    Added files to AD

---
+++ ActiveDirectory/Clear-KerberosTicket.ps1
@@ -0,0 +1,79 @@
+﻿<#        
+THIS CODE-SAMPLE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED    
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR    
+    FITNESS FOR A PARTICULAR PURPOSE.   
+      
+This sample is not supported under any Microsoft standard support program or service.    
+    The script is provided AS IS without warranty of any kind. Microsoft further disclaims all   
+    implied warranties including, without limitation, any implied warranties of merchantability   
+    or of fitness for a particular purpose. The entire risk arising out of the use or performance   
+    of the sample and documentation remains with you. In no event shall Microsoft, its authors,   
+    or anyone else involved in the creation, production, or delivery of the script be liable for    
+    any damages whatsoever (including, without limitation, damages for loss of business profits,    
+    business interruption, loss of business information, or other pecuniary loss) arising out of    
+    the use of or inability to use the sample or documentation, even if Microsoft has been advised    
+    of the possibility of such damages, rising out of the use of or inability to use the sample script,    
+    even if Microsoft has been advised of the possibility of such damages.    
+#>
+
+<#
+.Synopsis
+   This function uses Powershell remoting and the command line utility klist.exe to clear Kerberos Tickets
+.DESCRIPTION
+   This function uses Powershell remoting and the command line utility klist.exe to clear Kerberos Tickets
+
+   You can pass through multiple computer names and clear either the LocalSystem or NetworkService Kerberos Tickets.
+
+   This is a simplified script of the Technet/Microsoft script listed below:
+   http://blogs.technet.com/b/tspring/archive/2014/06/23/viewing-and-purging-cached-kerberos-tickets.aspx
+.EXAMPLE
+PS C:\windows\system32> Clear-KerberosTicket -ComputerName server01 -SessionName LocalSystem
+Success
+.EXAMPLE
+PS C:\windows\system32> Clear-KerberosTicket -ComputerName server01 -SessionName LocalSystem -Verbose
+VERBOSE: Attempting to connnect to server01
+WARNING: Unable to connect to server01
+#>
+function Clear-KerberosTicket
+{
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param
+    (
+        # Computer name variable
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $ComputerName,
+
+        # Session Name (Local System or Network Service)
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [ValidateSet('LocalSystem', 'NetworkService')]
+        $SessionName
+    )
+
+    Begin {
+        switch ($SessionName) {
+            'LocalSystem' { $ID = '0x3e7' }
+            'NetworkService' { $ID = '0x3e4' }
+            Default { Write-Error 'Invalid Session Name' -ErrorAction Stop}
+        }
+    } # End Begin
+    Process {
+        foreach ($Computer in $ComputerName) {
+            Write-Verbose "Attempting to connnect to $Computer"
+            If (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+                Write-Verbose "Clearing Kerberos Ticket on $Computer for $SessionName"
+                $Result = invoke-command -ComputerName $Computer -ScriptBlock { klist -li $Using:ID purge}
+                If ($Result[4].TrimStart() -like 'Ticket(s) Purged!') {
+                    Write-Output "Success - $Computer"
+                } Else {
+                    Write-Warning "Failure - $Computer"
+                } # End If
+            } Else {
+                Write-Warning "Unable to connect to $Computer"
+            } # End If
+        } # End Foreach
+    } # End Process
+} # End Clear-KerberosTicket
---
+++ GroupPolicy/Confirm-GPOBackup.ps1
@@ -0,0 +1,136 @@
+﻿<#
+.Synopsis
+   This script will perform (and utilize Convert-GUIDStoGPOName.ps1 to verify that all GPOs were backed up successfully.
+.DESCRIPTION
+   This script will perform (and utilize Convert-GUIDStoGPOName.ps1 to verify that all GPOs were backed up successfully.
+.EXAMPLE
+   .\Confirm-GPOBackup.ps1 -BackupPath C:\GPOBackup -Domain contoso.com
+.EXAMPLE
+   Another example of how to use this cmdlet
+.NOTES
+   This script is meant to be used with this script from Ian Farr
+      https://gallery.technet.microsoft.com/Comprehensive-Group-Policy-5f9d3ea6
+#>
+[CmdletBinding()]
+[Alias()]
+[OutputType([int])]
+Param
+(
+    # Specify the BackupFolder Path
+    [Parameter(Mandatory=$true,
+                ValueFromPipelineByPropertyName=$true,
+                Position=0)]
+    $BackupPath,
+
+    # Specify the domain to query
+    $Domain = $env:USERDOMAIN,
+
+    # Enter To address
+    $To = 'ADTeam@contoso.com',
+
+    # Enter From address
+    $From = 'no-reply@contoso.com',
+
+    # Enter SMTP Server
+    $SMTPServer = 'mail.contoso.com'
+    
+)
+
+Begin {
+    $Date = Get-Date -Format yyyy_MM_dd
+    #$Date = '2018_05_11'
+    Write-Verbose "Verifying access to $BackupPath\Date*"
+    If (-NOT(Test-Path -Path "$BackupPath\$Date*")){
+        Write-Error "Unable to access $BackupPath\$Date*" -ErrorAction Stop
+    } # End Test-Path
+    
+    Write-Verbose "Verifying access to $Domain"
+    If (-NOT(Test-Connection -ComputerName $Domain -Quiet -Count 2)){
+        Write-Error "Unable to access $Domain" -ErrorAction Stop
+    } # End Domain Check
+
+    Write-Verbose 'Verifying Group Policy module is available'
+    If ((Get-Module -ListAvailable -Name GroupPolicy -Verbose:$False) -eq $null){
+ 	    Throw 'GroupPolicy module is not available.'
+    } # End If 
+
+    Function Convert-GUIDStoGPOName {
+        <#
+        .SYNOPSIS
+           Query a specified GPO Backup Path and pull the display name
+        .DESCRIPTION
+           Query a specified GPO Backup Path and pull the display name
+        .EXAMPLE
+           .\Convert-GUIDStoGPOName.ps1 -Path C:\GPOBackup\2018_05_04_230022
+        #>
+        [CmdletBinding()]
+        [Alias()]
+        [OutputType([int])]
+        Param
+        (
+            # Param1 help description
+            [Parameter(Mandatory=$true,
+                        ValueFromPipelineByPropertyName=$true,
+                        Position=0)]
+            $Path
+        )
+
+        Begin {
+            If (-NOT(Test-Path $Path)){
+                Write-Error "Unable to reach $Path" -ErrorAction Stop
+            } # End Test-Path
+        } # End Begin
+        Process {
+
+
+            Write-Verbose "Pulling all backup.xml files from $Path"
+            $BackupFiles = Get-ChildItem -Path $Path -Recurse -Include Backup.xml
+
+            foreach ($File in $BackupFiles) {
+                $ResultHash = @{}
+
+                Write-Verbose "Query File and pull the XML content and get Display Name"
+                $GUID = $File.Directory.Name
+                $xml = [xml](Get-Content $File)
+                $DisplayName = $xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.DisplayName.InnerText
+                $ResultHash.add('DisplayName', $DisplayName)
+                $ResultHash.Add('ID', $GUID)
+
+                # Write Output
+                New-Object -TypeName PSObject -Property $ResultHash
+
+            } # End Foreach
+
+
+        } # End Process
+        End {
+            rv backupfiles, resulthash, guid, xml, displayname
+        } # End End
+
+    }
+
+} # End Begin
+Process{
+    $Backup = Convert-GUIDStoGPOName -Path "$BackupPath\$Date*"
+    $Prod = Get-GPO -All -Domain $Domain
+
+    $Comparison = Compare-Object -ReferenceObject $Prod.DisplayName -DifferenceObject $Backup.DisplayName
+
+    $BodyBUFail = "The following GPOs were in the backup, but not found in Production `n"
+    $BodyProdFail = "The following GPOs were in production, but not found in the Backup `n"
+
+    If ($Comparison -ne $null){
+        foreach ($GPO in $Comparison) {
+            If ($GPO.SideIndicator -eq '=>') {
+                $BodyBUFail += "$($GPO.InputObject) `n"
+            } ElseIf ($GPO.SideIndicateor -eq '<='){
+                $BodyProdFail += "$($GPO.InputObject) `n"
+            } # End If 
+        }
+        # We only want to send an e-mail if there was a backup anomaly
+        Send-MailMessage $To -From $From -SmtpServer $SMTPServer -Subject "GPO Backup Verification - $Date" -Body "$BodyBUFail`n$BodyProdFail"
+    }
+} # End Process
+End{
+    $Comparison
+} # End End
