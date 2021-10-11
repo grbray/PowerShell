b08426fb386497989b5d12b8a0a1b78c85ca7170
Graeme B <grbray@outlook.com>
2021-10-11 12:06:33 -0500

    Add Module Check

---
+++ Windows/Check-IfModuleExists.ps1
@@ -0,0 +1,6 @@
+﻿## This snippet will ensure that a module is available, correctly.
+
+Write-Verbose "Verifying ActiveDirectory module is available"
+If (-NOT(Get-Module -ListAvailable -Name ActiveDirectory -Verbose:$False)){
+ 	Throw 'ActiveDirectory module is not available.'
+} # End If 
